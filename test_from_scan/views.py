9b618a18fdde78a3986c44b3fa2a0b6d4a57163f
benjamindec <benjamin.decnuydt@student.uclouvain.be>
2017-12-22 00:32:46 +0100

    Sort questions corrected

--- test_from_scan/views.py
+++ test_from_scan/views.py
@@ -286,7 +286,7 @@ def lesson_test_from_scan_detail(request, lesson_pk, pk):
     questions = TestQuestionFromScan.objects.all().filter(test_id=pk).order_by('question_num')
     students = TestAnswerFromScan.objects.all().filter(test_id=pk).distinct('student_id').order_by('student_id','-is_correct')
 
-    if (not 'sort_question' in request.session) or (request.session['sort_question'] is None or request.session['sort_question'] == -1) or request.session['sort_question'] > len(questions):
+    if (not 'sort_question' in request.session) or request.session['sort_question'] is None or request.session['sort_question'] == -1:
         request.session['sort_question'] = -1;
         answers = TestAnswerFromScan.objects.all().filter(test_id=pk).order_by('id')
     else:
@@ -302,7 +302,9 @@ def lesson_test_from_scan_detail(request, lesson_pk, pk):
 
     if request.method == "POST":
         if 'questions' in request.POST:
+
             request.session['sort_question'] = int(request.POST.get('questions'))
+
         elif 'copy' in request.FILES:
 
             form = ImportCopyForm(request.POST, request.FILES)
